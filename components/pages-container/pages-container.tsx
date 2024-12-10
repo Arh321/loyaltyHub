09fb435d0b9f79b18caad3450718dc536d472d74
hasanzadeh <alirezawtf321@gmail.com>
2024-12-10 14:32:31 +0330

    fixing not found page and error boundary component

--- components/pages-container/pages-container.tsx
+++ components/pages-container/pages-container.tsx
@@ -1,5 +1,7 @@
 import AppLoading from "@/app/loading";
 import { Suspense } from "react";
+import ErrorBoundaryWrapper from "../error-component/ErrorBoundary";
+import NotFoundComponent from "../not-found-page/not-found-component";
 
 const PagesContainer = ({
   children,
@@ -10,7 +12,16 @@ const PagesContainer = ({
     <Suspense fallback={<AppLoading />}>
       <div className="w-full grow  no-scrollbar overflow-y-auto bg-BG rounded-t-[20px]  -mt-[12px]">
         <div className=" w-full min-h-[calc(100vh-80px)] relative  flex flex-col  pb-[100px]">
-          {children}
+          <ErrorBoundaryWrapper
+            fallback={
+              <NotFoundComponent
+                image={"@/public/LOGO.png"}
+                title="در هنگام برقراری ارتباط خطایی رخ داده است"
+              />
+            }
+          >
+            {children}
+          </ErrorBoundaryWrapper>
         </div>
       </div>
     </Suspense>
