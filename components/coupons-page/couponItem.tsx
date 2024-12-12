05c86b1ccc6345b9bd7fd440b2a7d081a627de32
hasanzadeh <alirezawtf321@gmail.com>
2024-12-13 01:41:19 +0330

    fixing couponItem component notife

--- components/coupons-page/couponItem.tsx
+++ components/coupons-page/couponItem.tsx
@@ -2,6 +2,7 @@ import { ICoupon } from "@/types/coupon-and-gift";
 import { numberToPersianPrice } from "@/utils/common-methods/number-to-price";
 import { CopyOutlined } from "@ant-design/icons";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { message } from "antd";
 import clsx from "clsx";
 import { useState } from "react";
 
@@ -16,17 +17,28 @@ export const CouponItem: React.FC<CouponItemProps> = ({
   index,
   type,
 }) => {
-  const [copySuccess, setCopySuccess] = useState<string>("");
+  const [messageApi, contextHolder] = message.useMessage();
+
   const copyToClipboard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
-      setCopySuccess("کپی شد");
+      messageApi.open({
+        type: "success",
+        content: "سریال کوپن کپی شد",
+        duration: 2,
+        style: {
+          zIndex: 999999999999,
+          fontFamily: "Medium",
+        },
+      });
     } catch (err) {
-      setCopySuccess("خطا در کپی");
+      messageApi.open({
+        type: "error",
+        content: "کپی کردن سریال با خطا مواجه شد",
+        duration: 2,
+        style: { zIndex: 999999999999, fontFamily: "Medium" },
+      });
     }
-    setTimeout(() => {
-      setCopySuccess("");
-    }, 2500);
   };
   return (
     <div
@@ -45,6 +57,7 @@ export const CouponItem: React.FC<CouponItemProps> = ({
         }
       )}
     >
+      {contextHolder}
       {type == "used" && (
         <div className="w-full h-full absolute top-0 right-0 flex justify-center items-center">
           <Icon
@@ -61,11 +74,6 @@ export const CouponItem: React.FC<CouponItemProps> = ({
         className="w-10 h-10 rounded-full border-Secondary border absolute left-4 top-4 bg-[rgb(30,156,81,0.1)] "
       >
         <CopyOutlined width={"2rem"} className="text-xl" />
-        {copySuccess && (
-          <span className="absolute  whitespace-nowrap rounded left-0 -bottom-8 w-max px-4 py-1 font-Medium text-Highlighter bg-[#48BB78] mx-auto font-regular text-highliter animate-fadeIn flex justify-center items-center">
-            {copySuccess}
-          </span>
-        )}
       </button>
       <div className="bg-[rgb(30,156,81,0.1)] rounded-full w-16 h-16 flex flex-col justify-center items-center gap-1 text-Secondary2">
         <span className="font-Medium text-lg">{coupon.reduction}%</span>
